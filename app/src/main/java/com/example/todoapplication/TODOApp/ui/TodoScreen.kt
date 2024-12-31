501694410d189623ae944b9dc231e8851245d1d7
dnuccio80 <nuccio227@gmail.com>
2024-12-31 15:04:16 -0300

    TodoScreen created

--- app/src/main/java/com/example/todoapplication/TODOApp/ui/TodoScreen.kt
+++ app/src/main/java/com/example/todoapplication/TODOApp/ui/TodoScreen.kt
@@ -3,11 +3,14 @@ package com.example.todoapplication.TODOApp.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun TodoScreen(){
-    Box(Modifier.fillMaxSize().background(Color.Red))
+    Box(Modifier.fillMaxSize().background(Color.Red)) {
+        Text(text = "Hello World")
+    }
 }
